4785d75ccaf6ba9b69685671c48a88f0c3e4119e
Starr Parker <starrparker@Starrs-MacBook-Pro-2.local>
2018-05-06 21:53:35 -0700

    initial add

---
+++ test/src/main/java/package/test.java
@@ -0,0 +1,10 @@
+package test.src.main.java;
+
+class test{
+    public static void Main(string[] args){
+        if(args != null && args[0] != null)
+        {
+            System.Console.WriteLine("Hello " + args[0]);
+        }
+    }
+}
---
+++ test/src/main/java/service/test.java
@@ -0,0 +1,9 @@
+package service;
+
+public class test {
+	public static void main(String[] args) {
+		if (args != null && args[0] != null) {
+			System.out.println("Hello " + args[0]);
+		}
+	}
+}

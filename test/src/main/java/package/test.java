219daf9374ead9f876e986067afca5a85691fff7
starrparker <starrparker@live.com>
2018-05-06 22:16:01 -0700

    making sure we have connection

--- test/src/main/java/package/test.java
+++ test/src/main/java/package/test.java
@@ -4,6 +4,7 @@ class test{
     public static void Main(string[] args){
         if(args != null && args[0] != null)
         {
+            //this is a hello world app
             System.Console.WriteLine("Hello " + args[0]);
         }
     }
